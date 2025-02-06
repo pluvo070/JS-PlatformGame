1b6dc1f02e43b38828f89a2c154c8797ced00eba
pluvo070 <1478706539@qq.com>
2025-02-06 19:55:49 +0000

    测试脚本-设置全局变量用于测试

---
+++ jest.setup.js
@@ -0,0 +1,32 @@
+// 设置所有要用到的全局变量
+global.tileSize = 1;
+global.tilemargin = 0;
+global.windowHeight = 350; 
+global.windowWidth = 500;
+
+global.levelHeight = []; 
+global.levelWidth = [];
+
+global.offsetX = 0;  // 水平偏移
+global.offsetY = 0;  // 垂直偏移
+
+
+global.frameInterval = 18; 
+
+global.keys = {};
+global.messages = [];
+
+
+global.coll = []; 
+global.others = [];
+global.water = [];
+
+global.player = []; 
+
+global.flag = []; 
+global.enemies = [];
+global.traps = []; 
+global.diamonds = [];
+global.boxes = [];
+
+global.selectedLevel = 0;
